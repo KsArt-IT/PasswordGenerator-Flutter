520575840541c71ee87e77f470439244713ec27a
KsArT-IT <ksart.it@gmail.com>
2025-02-12 15:07:12 +0200

    feat: add constants and dimensions for password generation

---
+++ lib/models/constants.dart
@@ -0,0 +1,10 @@
+interface class Constants {
+  // Length of the password
+  static const minLength = 6;
+  static const maxLength = 20;
+  // Characters to use in the password
+  static const capitalLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  static const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+  static const numbers = '0123456789';
+  static const specialCharacters = '!@#%^&*()';
+}
---
+++ lib/ui/theme/dimens.dart
@@ -0,0 +1,7 @@
+interface class Dimens {
+  static const paddingVertical = 16.0;
+  static const padding = 4.0;
+  static const borderRadius = 4.0;
+  static const spacing = 4.0;
+  static const border = 2.0;
+}
